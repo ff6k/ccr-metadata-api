ac171185215d48743d15f0b8514ae3da6f902d55
Cooler <75097898+coolerwind@users.noreply.github.com>
2021-12-09 05:55:48 +0800

    update root url

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 const PROVIDER = 'https://volta-rpc.energyweb.org'
 const CONTRACT_ADDRESS = '0x5110e879E6b3dad2Fe35839EE7ab2118bAd5dcA1'
-const HOST_URL="http://localhost:5000"
+const HOST_URL="https://ccr-metadata.herokuapp.com"
 
 module.exports = {
   PROVIDER,
