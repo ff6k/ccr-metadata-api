832b0bc6e66c6b4ecda260383f5cd24408889d3d
Cooler <75097898+coolerwind@users.noreply.github.com>
2021-12-09 05:47:20 +0800

    first commit

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+const PROVIDER = 'https://volta-rpc.energyweb.org'
+const CONTRACT_ADDRESS = '0x5110e879E6b3dad2Fe35839EE7ab2118bAd5dcA1'
+const HOST_URL="http://localhost:5000"
+
+module.exports = {
+  PROVIDER,
+  CONTRACT_ADDRESS,
+  HOST_URL
+}
