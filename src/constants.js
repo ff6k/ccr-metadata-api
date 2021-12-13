93f6dc0cfaf48e9ecf03c747cc1ffc3cbb07def2
Cooler <75097898+coolerwind@users.noreply.github.com>
2021-12-14 02:39:45 +0800

    pacakge updates

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 const PROVIDER = 'https://volta-rpc.energyweb.org'
-const CONTRACT_ADDRESS = '0x706807ACd48Fc9c2DcE467c33C65E4234Ba6f382'
+const CONTRACT_ADDRESS = '0x9e363D58A856f32F842fB3e93EBB405f2BB887FD'
 const HOST_URL="https://ccr-metadata.herokuapp.com"
 
 module.exports = {
