e5687be02944a0c25964bb8cf182e548946e3f2a
Cooler <75097898+coolerwind@users.noreply.github.com>
2022-01-25 21:20:25 +0800

    add provider at function

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 const PROVIDER = 'wss://volta-rpc.energyweb.org/ws'
-const CCR_CONTRACT_ADDRESS = '0x38B7D4e5be43CB9ab2E8dA51D90eD755168eb09f'
+const CCR_CONTRACT_ADDRESS = '0x7Db718FbcD4c74a2d0f98597A8178Edc4EEd26F8'
 const HOST_URL="https://ccr-metadata.herokuapp.com"
 const CRC_CONTRACT_ADDRESS = "0x8Bb7381a67AcA5bbf8092bbF576813F42e221b3D"
 
