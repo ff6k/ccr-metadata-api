77041114df9320fc886cee8db099770d16305c0e
Cooler <75097898+coolerwind@users.noreply.github.com>
2022-02-20 06:33:36 -0800

    change crc contract address

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 const PROVIDER = 'wss://volta-rpc.energyweb.org/ws'
 const CCR_CONTRACT_ADDRESS = '0xf7A602e61FeCe08ebFB2d39022071ba5C05918FC'
 const HOST_URL="https://ccr-metadata.herokuapp.com"
-const CRC_CONTRACT_ADDRESS = "0x38692FF6E1165Bdc0139a02B05EaeAB83F1c5851"
+const CRC_CONTRACT_ADDRESS = "0x7a458DC4C873aecad7DCE68D4e895FDB7233d940"
 
 module.exports = {
   CCR_CONTRACT_ADDRESS,
